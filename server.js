027b9cbcf9b8d81b2ca58bb6224a0757c2b95dfd
Glitch (file-metadata-ajb) <none>
2018-06-01 10:14:20 +0000

    🌨💸 Checkpoint ./server.js:1080052/160 ./public/style.css:1080052/1

--- server.js
+++ server.js
@@ -11,16 +11,11 @@ const express = require('express'),
 app.use(bodyParser.json());
 app.use(cors());
 
-//app.get("/", (req, res, next) => {
-  //res.sendFile(__dirname + '/views/index.html')
-//})
-
 app.use(express.static(__dirname + '/public'));
 
 app.post('/upload', upload.single('file'), (req, res, next) => {
-  
-  return res.json(req.file);  
-})
+  return res.json(req.file); 
+});
 
 // listen for requests :)
 const listener = app.listen("3000", () => {
